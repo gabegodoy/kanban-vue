ed5e0ff93214d45d185986f75d48415f9c76b4a0
godoy-nexum <gabriel.godoy@nexum.com.br>
2024-03-31 11:05:03 -0300

    feat: :construction: Base Collumn for Board

---
+++ src/components/BoardColumn.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+defineProps<{ title: string }>()
+</script>
+
+<template>
+    <div class="flex flex-col gap-4 w-80">
+        <!-- Board Column Title -->
+        <div class="flex items-center justify-between w-full h-11 px-2 py-1 border-teal-300 border-t-4 shadow rounded uppercase font-bold text-gray-500">
+            <p>{{title}}</p>
+            <font-awesome-icon class="text-teal-300 cursor-pointer" icon="plus" />
+        </div>
+        <slot></slot>
+    </div>
+</template>
