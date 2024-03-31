02ffa34c2d1b1c5836726e158e4bd0706131f159
godoy-nexum <gabriel.godoy@nexum.com.br>
2024-03-31 11:04:08 -0300

    feat: :heavy_plus_sign: ADD FontAwesome to the Project

--- src/main.ts
+++ src/main.ts
@@ -2,4 +2,13 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+const app = createApp(App)
+library.add(fas)
+app.component('FontAwesomeIcon', FontAwesomeIcon)
+
+app.mount('#app')
+
